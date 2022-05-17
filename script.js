669cf0e16799a30bd57455063757f82fc515d1ee
soledademaria <solmmaria50@gmail.com>
2022-05-16 22:33:48 -0300

    feat(script): add variaveis dinamicas

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputElement = document.querySelector(".new-task-input");
+const addTaskButton = document.querySelector(".new-task-button");
+const tasksContainer = document.querySelector(".tasks-container");
+const validateInput = () => inputElement.value.trim().length > 0;
+
+
