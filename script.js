964086699221a44cd95e0d5c67dec0a5329afe0d
soledademaria <solmmaria50@gmail.com>
2022-05-16 22:34:41 -0300

    feat(script): create handleAddTaskFunction

--- script.js
+++ script.js
@@ -3,4 +3,34 @@ const addTaskButton = document.querySelector(".new-task-button");
 const tasksContainer = document.querySelector(".tasks-container");
 const validateInput = () => inputElement.value.trim().length > 0;
 
-
+const handleAddTask = () => {
+    const inputValid = validateInput();
+  
+    if (!inputValid) {
+      return inputElement.classList.add("error");
+    }
+  
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+  
+    const taskContent = document.createElement("p");
+    taskContent.innerText = inputElement.value;
+  
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+  
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+    deleteItem.classList.add("delete-task-button");
+  
+    deleteItem.addEventListener("click", () => handleDelete(taskItemContainer, taskContent));
+  
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+  
+    tasksContainer.appendChild(taskItemContainer);
+  
+    inputElement.value = "";
+  };
+  
+ 
